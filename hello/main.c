ece7aa6dabcfe32b071547906bda8a0a661a2eed
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-08-20 15:24:46 -0700

    first commit

---
+++ hello/main.c
@@ -0,0 +1,12 @@
+#include <SDL.h>
+
+int main( int argc, char *argv[] )
+{
+
+	SDL_ShowSimpleMessageBox( SDL_MESSAGEBOX_INFORMATION, 
+              "Hello World",
+              "You have successfully compiled and linked an SDL2"
+              " program, congratulations.", NULL );
+
+	return 0;
+}
